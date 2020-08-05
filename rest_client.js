ae0ca70e5de72491c74fbff997760f54fb6d7764
Cássia <cascrisbern@gmail.com>
2020-08-05 12:13:36 -0300

    used axios to make requests to rest-api-example

---
+++ rest_client.js
@@ -0,0 +1,65 @@
+const axios = require('axios');
+
+async function getEntity(url) {
+  try {
+    const response = await axios.get(url);
+    console.log('response', response.data);
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
+async function postEntity(url) {
+  axios.post(url, {
+    id: 10,
+    name: 'Boootcamp',
+    author: 'BootLearn',
+    price: 1500
+  })
+  .then(function (response) {
+    console.log('response', response.data[response.data.length-1]);
+  })
+  .catch(function (error) {
+    console.log('error', error);
+  });
+}
+
+async function putEntity(url) {
+  axios.put(url, {
+    id: 13,
+    name: 'Node Modification',
+    author: 'Cris',
+    price: 150
+  })
+  .then(function (response) {
+    console.log('response', response.data);
+  })
+  .catch(function (error) {
+    console.log('error', error);
+  });
+}
+
+async function deleteEntity(url) {
+  axios.delete(url, {
+    id: 8,
+    name: 'Node Modification',
+    author: 'Cris',
+    price: 150
+  })
+  .then(function (response) {
+    console.log('response', response.data);
+  })
+  .catch(function (error) {
+    console.log('error', error);
+  });
+}
+
+
+async function main() {
+  //getEntity("http://localhost:8080/courses") 
+  //postEntity("http://localhost:8080/courses/create") 
+  putEntity("http://localhost:8080/courses/13") 
+  //deleteEntity("http://localhost:8080/courses/8") 
+}
+
+main()
